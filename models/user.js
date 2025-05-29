373c0f87125137bfb493742d551db593536eae0c
benw0822 <benw822@gmail.com>
2025-05-29 20:57:21 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -7,10 +7,12 @@ const userSchema = new mongoose.Schema({
   name:     { type: String },
   points:   { type: Number, default: 0 },
   points2:  { type: Number, default: 0 },
-  gender:   { type: String },         // 新增
-  birthday: { type: String },         // 新增
-  address:  { type: String },         // 新增
-  note:     { type: String },         // 新增
+  gender:   { type: String },
+  birthday: { type: String }, // 格式如 "1995-01-01"
+  address:  { type: String },
+  city:     { type: String },      // 新增
+  inviteCode: { type: String },    // 新增
+  note:     { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
